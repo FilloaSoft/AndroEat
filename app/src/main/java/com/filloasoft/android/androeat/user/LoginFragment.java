98275cfd72ac09e442c8dd6e5f7a0388b22363dd
p.juane <p.juane@udc.es>
2019-04-05 18:35:45 +0200

    Login y registro de usuarios en BD

--- app/src/main/java/com/filloasoft/android/androeat/user/LoginFragment.java
+++ app/src/main/java/com/filloasoft/android/androeat/user/LoginFragment.java
@@ -1,19 +1,43 @@
 package com.filloasoft.android.androeat.user;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.NestedScrollView;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatTextView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.filloasoft.android.androeat.R;
+import com.filloasoft.android.androeat.helpers.InputValidation;
+import com.filloasoft.android.androeat.recipe.HomeFragment;
+import com.filloasoft.android.androeat.sql.DatabaseHelper;
 
-public class LoginFragment extends Fragment {
+public class LoginFragment extends Fragment implements View.OnClickListener {
 
     Toast toast;
+
+    private AppCompatButton appCompatButtonLogin;
+
+    private AppCompatButton appCompatButtonRegister;
+
+    private TextInputLayout textInputLayoutEmail;
+    private TextInputLayout textInputLayoutPassword;
+
+    private TextInputEditText textInputEditTextEmail;
+    private TextInputEditText textInputEditTextPassword;
+
+    private InputValidation inputValidation;
+    private DatabaseHelper databaseHelper;
 
     @Nullable
     @Override
@@ -27,10 +51,91 @@ public class LoginFragment extends Fragment {
         toast.show();
 
         BottomNavigationView navigation = loginView.findViewById(R.id.navigation);
-        //just change the fragment_dashboard
-        //with the fragment you want to inflate
-        //like if the class is HomeFragment it should have R.layout.home_fragment
-        //if it is DashboardFragment it should have R.layout.fragment_dashboard
+
+        textInputLayoutEmail = (TextInputLayout) loginView.findViewById(R.id.textInputLayoutEmail);
+        textInputLayoutPassword = (TextInputLayout) loginView.findViewById(R.id.textInputLayoutPassword);
+
+        textInputEditTextEmail = (TextInputEditText) loginView.findViewById(R.id.email_text);
+        textInputEditTextPassword = (TextInputEditText) loginView.findViewById(R.id.password_text);
+        appCompatButtonLogin = (AppCompatButton) loginView.findViewById(R.id.button);
+        appCompatButtonRegister = (AppCompatButton) loginView.findViewById(R.id.button2);
+
+        appCompatButtonLogin.setOnClickListener(this);
+        appCompatButtonRegister.setOnClickListener(this);
+
+        databaseHelper = new DatabaseHelper(getActivity());
+        inputValidation = new InputValidation(getActivity());
+
         return loginView;
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                verifyFromSQLite();
+                //TODO: falta que cuando el usuario se loguee correctamente se lance un mensaje de éxito
+                //Antes de acceder a HomeFragment
+                HomeFragment homeFragment = new HomeFragment();
+                loadFragment(homeFragment,false);
+                break;
+            case R.id.button2:
+                // Navigate to SignupFragment
+                SignupFragment newSignupFragment = new SignupFragment();
+                loadFragment(newSignupFragment, false);
+                break;
+        }
+    }
+
+    private boolean loadFragment(Fragment fragment, boolean firstFragment) {
+        //switching fragment
+        if (fragment != null) {
+            FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+
+            transaction.replace(R.id.fragment_container, fragment);
+            if (!firstFragment) {
+                transaction.addToBackStack(null);
+            }
+            transaction.commit();
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * This method is to validate the input text fields and verify login credentials from SQLite
+     */
+    private void verifyFromSQLite() {
+
+        if (!inputValidation.isInputEditTextFilled(textInputEditTextEmail, textInputLayoutEmail, "Email not filled")) {
+            return;
+        }
+        if (!inputValidation.isInputEditTextEmail(textInputEditTextEmail, textInputLayoutEmail, "Email not valid")) {
+            return;
+        }
+        if (!inputValidation.isInputEditTextFilled(textInputEditTextPassword, textInputLayoutPassword, "Password not filled")) {
+            return;
+        }
+
+        if (databaseHelper.checkUser(textInputEditTextEmail.getText().toString().trim()
+                , textInputEditTextPassword.getText().toString().trim())) {
+        //TODO: falta que cuando el usuario se loguee correctamente se lance un mensaje de éxito
+            toast = Toast.makeText(getActivity(),"Logged in succesfully!", Toast.LENGTH_SHORT);
+            toast.show();
+            emptyInputEditText();
+            //Access to Home Fragment (inside the case)
+        } else {
+            toast = Toast.makeText(getActivity(),
+                    "Credenciales inválidas", Toast.LENGTH_SHORT);
+        }
+    }
+
+    /**
+     * This method is to empty all input edit text
+     */
+    private void emptyInputEditText() {
+        textInputEditTextEmail.setText(null);
+        textInputEditTextPassword.setText(null);
     }
 }
--- app/src/main/java/com/filloasoft/android/androeat/user/SignupFragment.java
+++ app/src/main/java/com/filloasoft/android/androeat/user/SignupFragment.java
@@ -3,7 +3,12 @@ package com.filloasoft.android.androeat.user;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.AppCompatButton;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,11 +16,31 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.filloasoft.android.androeat.R;
+import com.filloasoft.android.androeat.helpers.InputValidation;
+import com.filloasoft.android.androeat.model.User;
+import com.filloasoft.android.androeat.recipe.HomeFragment;
+import com.filloasoft.android.androeat.sql.DatabaseHelper;
 
-public class SignupFragment extends Fragment {
+public class SignupFragment extends Fragment implements View.OnClickListener {
 
     Toast toast;
     public EditText editText;
+
+    private AppCompatButton appCompatButtonRegister;
+
+    private TextInputLayout textInputLayoutName;
+    private TextInputLayout textInputLayoutEmail;
+    private TextInputLayout textInputLayoutPassword;
+    private TextInputLayout textInputLayoutConfirmPassword;
+
+    private TextInputEditText textInputEditTextName;
+    private TextInputEditText textInputEditTextEmail;
+    private TextInputEditText textInputEditTextPassword;
+    private TextInputEditText textInputEditTextConfirmPassword;
+
+    private InputValidation inputValidation;
+    private DatabaseHelper databaseHelper;
+    private User user;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -24,6 +49,100 @@ public class SignupFragment extends Fragment {
                 "Pantalla de registro!", Toast.LENGTH_SHORT);
         toast.show();
 
+        textInputLayoutName = (TextInputLayout) signupView.findViewById(R.id.textInputLayoutName);
+        textInputLayoutEmail = (TextInputLayout) signupView.findViewById(R.id.textInputLayoutEmail);
+        textInputLayoutPassword = (TextInputLayout) signupView.findViewById(R.id.textInputLayoutPassword);
+        textInputLayoutConfirmPassword = (TextInputLayout) signupView.findViewById(R.id.textInputLayoutConfirmPassword);
+
+
+        textInputEditTextName = (TextInputEditText) signupView.findViewById(R.id.username_singup);
+        textInputEditTextEmail = (TextInputEditText) signupView.findViewById(R.id.email_text_singup);
+        textInputEditTextPassword = (TextInputEditText) signupView.findViewById(R.id.password_text_singup);
+        textInputEditTextConfirmPassword = (TextInputEditText) signupView.findViewById(R.id.password_text_singup2);
+        appCompatButtonRegister = (AppCompatButton) signupView.findViewById(R.id.button_singup_2);
+
+        appCompatButtonRegister.setOnClickListener(this);
+
+        inputValidation = new InputValidation(getActivity());
+        databaseHelper = new DatabaseHelper(getActivity());
+        user = new User();
+
         return signupView;
     }
+
+    @Override
+    public void onClick(View signupView) {
+        postDataToSQLite();
+   }
+
+
+    /**
+     * This method is to validate the input text fields and post data to SQLite
+     */
+    private void postDataToSQLite() {
+        if (!inputValidation.isInputEditTextFilled(textInputEditTextName, textInputLayoutName, "Name is not filled")) {
+            return;
+        }
+        if (!inputValidation.isInputEditTextFilled(textInputEditTextEmail, textInputLayoutEmail, "Email is not filled")) {
+            return;
+        }
+        if (!inputValidation.isInputEditTextEmail(textInputEditTextEmail, textInputLayoutEmail, "Email is not valid")) {
+            return;
+        }
+        if (!inputValidation.isInputEditTextFilled(textInputEditTextPassword, textInputLayoutPassword, "Password is not filled")) {
+            return;
+        }
+        if (!inputValidation.isInputEditTextMatches(textInputEditTextPassword, textInputEditTextConfirmPassword,
+                textInputLayoutConfirmPassword, "Password does not match")) {
+            return;
+        }
+
+        if (!databaseHelper.checkUser(textInputEditTextEmail.getText().toString().trim())) {
+
+            user.setName(textInputEditTextName.getText().toString().trim());
+            user.setEmail(textInputEditTextEmail.getText().toString().trim());
+            user.setPassword(textInputEditTextPassword.getText().toString().trim());
+
+            databaseHelper.addUser(user);
+
+            // show success message that record saved successfully
+            toast = Toast.makeText(getActivity(),"Registered succesfully!", Toast.LENGTH_SHORT);
+            toast.show();
+            emptyInputEditText();
+            HomeFragment homeFragment = new HomeFragment();
+            loadFragment(homeFragment,false);
+
+        } else {
+            // show error message that record already exists
+            toast = Toast.makeText(getActivity(),"Mail already exists", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+    }
+
+    private boolean loadFragment(Fragment fragment, boolean firstFragment) {
+        //switching fragment
+        if (fragment != null) {
+            FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+
+            transaction.replace(R.id.fragment_container, fragment);
+            if (!firstFragment) {
+                transaction.addToBackStack(null);
+            }
+            transaction.commit();
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * This method is to empty all input edit text
+     */
+    private void emptyInputEditText() {
+        textInputEditTextName.setText(null);
+        textInputEditTextEmail.setText(null);
+        textInputEditTextPassword.setText(null);
+        textInputEditTextConfirmPassword.setText(null);
+    }
+
 }
