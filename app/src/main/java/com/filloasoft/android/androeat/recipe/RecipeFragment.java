0529d239747ef774466202d16083a05f250574fc
adrian89rodriguezb <adrian.rodriguezb@udc.es>
2019-05-14 01:24:55 +0200

    Compartir receta
    Compartir receta

--- app/src/main/java/com/filloasoft/android/androeat/recipe/RecipeFragment.java
+++ app/src/main/java/com/filloasoft/android/androeat/recipe/RecipeFragment.java
@@ -14,6 +14,7 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.app.ShareCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewCompat;
 import android.support.v4.view.ViewPager;
@@ -93,6 +94,20 @@ public class RecipeFragment extends Fragment {
                 }
                 favouriteButton.setImageTintList(ColorStateList.valueOf(color));
 //                favouriteButton.setBackgroundTintList(ColorStateList.valueOf(color));
+            }
+        });
+
+
+        final FloatingActionButton shareButton = view.findViewById(R.id.shareButton);
+
+        shareButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ShareCompat.IntentBuilder.from(getActivity())
+                        .setType("text/plain")
+                        .setChooserTitle("Share Recipe")
+                        .setText("http://www.google.es")
+                        .startChooser();
             }
         });
 
